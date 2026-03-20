5af4e12fae6342dbfcca8800dc51a9e7ec4a47db
aryabovich <aryabovich@uoc.edu>
2026-03-20 18:35:33 +0100

    Boton Cerrar
    Boton Cerrar

--- src/app/components/detail/detail.ts
+++ src/app/components/detail/detail.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,11 +10,16 @@ import { CommonModule } from '@angular/common';
 })
 export class DetailComponent implements OnChanges {
   @Input() selectedPlayer: any = null;
+  @Output() clearSelection = new EventEmitter<void>();
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['selectedPlayer'] && this.selectedPlayer) {
       document.documentElement.style.setProperty('--color1', this.selectedPlayer.color1);
       document.documentElement.style.setProperty('--color2', this.selectedPlayer.color2);
     }
+  }
+
+  onClearSelection(): void {
+    this.clearSelection.emit();
   }
 }
